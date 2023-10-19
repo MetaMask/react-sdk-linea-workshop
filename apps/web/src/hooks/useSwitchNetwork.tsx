59167087f8dbb0837baf7a39e258ac357967d877
httpjunkie <eric@httpjunkie.com>
2023-10-19 13:01:03 -0400

    Update SwitchNetwork component to use try catch.

--- apps/web/src/hooks/useSwitchNetwork.tsx
+++ apps/web/src/hooks/useSwitchNetwork.tsx
@@ -7,25 +7,38 @@ export const useSwitchNetwork = () => {
     if(!isSupportedNetwork(networkId)) {
       throw new Error('Unsupported network')
     }
-  
-    await window.ethereum?.request({
-      method: 'wallet_addEthereumChain',
-      params: [
-        {
-          chainId: networkId,
-          ...(config[networkId].blockExplorer ? {
-            blockExplorerUrls: [config[networkId].blockExplorer]
-          } : {}),
-          chainName: config[networkId].name,
-          nativeCurrency: {
-            decimals: 18,
-            name: config[networkId].name,
-            symbol: config[networkId].symbol,
-          },
-          rpcUrls: [config[networkId].rpcUrl],
-        },
-      ],
-    })
+
+    try {
+      await window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: networkId, }],
+      })
+    } catch (error) {
+      try {
+        await window.ethereum?.request({
+          method: 'wallet_addEthereumChain',
+          params: [
+            {
+              chainId: networkId,
+              ...(config[networkId].blockExplorer ? {
+                blockExplorerUrls: [config[networkId].blockExplorer]
+              } : {}),
+              chainName: config[networkId].name,
+              nativeCurrency: {
+                decimals: 18,
+                name: config[networkId].name,
+                symbol: config[networkId].symbol,
+              },
+              rpcUrls: [config[networkId].rpcUrl],
+            },
+          ],
+        })
+      } catch (error) {
+        // user rejects the request to "add chain" or param values are wrong, maybe you didn't use hex above for `chainId`?
+        console.log(`wallet_addEthereumChain Error: ${error.message}`)
+      }
+      // handle other "switch" errors
+    }
 
   }
 
