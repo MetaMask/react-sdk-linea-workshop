a22a083c9a947b28a1e1c5d1eaab9245b160d8e9
httpjunkie <eric@httpjunkie.com>
2023-10-31 00:57:23 -0400

    updated for @web3-onboard/metamask

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -7,6 +7,8 @@ import { MetaMaskError } from './components/MetaMaskError'
 import { AppContextProvider } from './hooks/useAppContext'
 
 // Web3 Onboard Imports
+// import Onboard from '@web3-onboard/core'
+import metamaskSDK from '@web3-onboard/metamask'
 import { Web3OnboardProvider, init } from '@web3-onboard/react'
 import injectedModule from '@web3-onboard/injected-wallets'
 const INFURA_KEY = import.meta.env.VITE_PUBLIC_INFURA_PROJECT_ID
@@ -16,10 +18,26 @@ const ethereumLinea = {
   label: 'Linea Testnet',
   rpcUrl: `https://linea-goerli.infura.io/v3/${INFURA_KEY}`
 }
+
+/* ADDED FOR @web3-onboard/metamask */
+const mmSdkOptions = {
+  logging: { developerMode: false },
+  checkInstallationImmediately: false, // This will automatically connect to MetaMask on page load
+  dappMetadata: {
+    name: 'Demo React App',
+    url: window.location.host,
+  },
+}
+const metamaskSDKWallet = metamaskSDK({options: mmSdkOptions})
+// const onboard = Onboard({
+//   wallets: [metamaskSDKWallet]
+// })
+/* ADDED FOR @web3-onboard/metamask */
+
 const chains = [ethereumLinea]
 const wallets = [injectedModule()]
 const web3Onboard = init({
-  wallets,
+  wallets: [metamaskSDKWallet], /* UPDATED FOR @web3-onboard/metamask */
   chains,
   appMetadata: {
     name: 'ETH Atlantis',
