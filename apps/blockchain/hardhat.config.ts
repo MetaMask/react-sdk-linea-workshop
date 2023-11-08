1300c28bbacb36bf49bfa05c9e80cd1d7c22363a
httpjunkie <eric@httpjunkie.com>
2023-11-08 14:24:26 -0500

    Update README, hardhat config and env.examples

--- apps/blockchain/hardhat.config.ts
+++ apps/blockchain/hardhat.config.ts
@@ -1,8 +1,11 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import '@typechain/hardhat';
 import dotenv from "dotenv";
 dotenv.config();
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import '@typechain/hardhat'
+import '@nomicfoundation/hardhat-ethers'
+import '@nomicfoundation/hardhat-chai-matchers'
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -13,11 +16,11 @@ const config: HardhatUserConfig = {
   },
   networks: {
     mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY!],
     },
     lineaTestnet: {
-      url: `https://linea-goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      url: `https://linea-goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY!],
     },
   },
