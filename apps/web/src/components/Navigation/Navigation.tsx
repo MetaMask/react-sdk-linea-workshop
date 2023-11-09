fb01e2c5929ae28706951404154129312e2ae8f4
httpjunkie <eric@httpjunkie.com>
2023-11-08 23:01:59 -0500

    update navigation

--- apps/web/src/components/Navigation/Navigation.tsx
+++ apps/web/src/components/Navigation/Navigation.tsx
@@ -9,12 +9,12 @@ import { isSupportedNetwork } from '~/lib/isSupportedNetwork'
 export const Navigation = () => {
   const { wallet, isConnecting, connectMetaMask, sdk, sdkConnected } =
     useMetaMask()
-  const networkId = import.meta.env.VITE_PUBLIC_CHAIN_ID
+  const chainId = import.meta.env.VITE_PUBLIC_CHAIN_ID
   const walletChainSupported = isSupportedNetwork(wallet.chainId)
 
   // now chainInfo is strongly typed or fallback to linea if not a valid chain
-  const chainInfo = isSupportedNetwork(networkId)
-    ? config[networkId]
+  const chainInfo = isSupportedNetwork(chainId)
+    ? config[chainId]
     : config['0xe704']
 
   return (
